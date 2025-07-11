a44f2cd9f059026055f94998d714e4fd6cefea9e
Yeder <ypimentel.tapia@gmail.com>
2025-07-12 00:16:57 +0200

    feat(logic) : applied the logic of the book project

---
+++ sistema_libros/libros/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Libro
+
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'fecha_publicacion', 'isbn')
+    search_fields = ('titulo', 'autor', 'isbn')
+    list_filter = ('fecha_publicacion',)
